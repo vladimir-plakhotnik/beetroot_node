adda59ca7c2bd46cd5fd89fb1b451dfcb40ed046
Vladimir Plakhotnik <vn.plakhotnik@outlook.com>
2020-07-03 00:48:10 +0300

    Deleted previous homework

--- homework/01/person/index.js
+++
@@ -1,6 +0,0 @@
-const { Person } = require('./person');
-const { test } = require('./app');
-
-module.exports = { 
-    Person, test 
-}
--- homework/02/app.js
+++
@@ -1,7 +0,0 @@
-const { writeCmdFile } = require('./writeCmdFile');
-
-(async function test() {
-    await writeCmdFile("test.txt", "This is the test 1");
-    await writeCmdFile("test.txt", "This is the test 2");
-    await writeCmdFile("test.txt", "This is the test 3");
-})();
